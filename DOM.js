407e3ff44c6f05cf5c9862b70cd623f5171968b6
Jerlinreeta <jerlinreeta214@gmail.com>
2026-07-02 12:00:31 +0530

    Remove DOM.js

--- DOM.js
+++
@@ -1,8 +0,0 @@
-
-function add =()=>{
-let a = document.getElementById("inp1").value
-let b = document.getElementById("inp1").value
-let c = a+b;
-console.log(c);
-}
-document.getElementsById('btn',add);
