cc09a50ad1157dad1ca974888bcc0a922bdbc327
Jerlinreeta <jerlinreeta214@gmail.com>
2026-07-02 11:48:37 +0530

    Initial commit

---
+++ DOM.js
@@ -0,0 +1,8 @@
+
+function add =()=>{
+let a = document.getElementById("inp1").value
+let b = document.getElementById("inp1").value
+let c = a+b;
+console.log(c);
+}
+document.getElementsById('btn',add);
---
+++ student.js
@@ -0,0 +1,8 @@
+let name="Jerlin";
+function personal_data(){
+    let rollNo =102;
+    let classes = "A section";
+    console.log(rollno)
+}
+console.log(name)
+
